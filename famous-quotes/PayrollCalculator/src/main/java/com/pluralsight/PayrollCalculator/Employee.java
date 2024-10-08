537a0fe09bb1784bb0ae55ac8d56b7a587de5a83
deibcr <deibcr@gmail.com>
2024-10-08 14:55:02 -0400

    first commit

---
+++ famous-quotes/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator/Employee.java
@@ -0,0 +1,53 @@
+package com.pluralsight.PayrollCalculator;
+
+public class Employee {
+    int employeeID;
+    String name;
+    double hoursWorked;
+    double payRate;
+
+    public Employee(int employeeID, String name, double hoursWorked, double payRate) {
+        this.employeeID = employeeID;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getGrossPay(){
+        return hoursWorked*payRate;
+    }
+
+
+}
---
+++ famous-quotes/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator/PayRollCalculator.java
@@ -0,0 +1,9 @@
+package com.pluralsight.PayrollCalculator;
+
+public class PayRollCalculator {
+    public static void main(String[] args) {
+
+
+
+    }
+}
