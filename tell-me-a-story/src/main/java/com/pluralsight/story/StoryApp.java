d54ee8f14f29413a83168e6a953b9994a89861fc
deibcr <deibcr@gmail.com>
2024-10-08 13:32:21 -0400

    2nd commit

--- tell-me-a-story/src/main/java/com/pluralsight/story/StoryApp.java
+++ tell-me-a-story/src/main/java/com/pluralsight/story/StoryApp.java
@@ -5,22 +5,47 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class StoryApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner pScanner = new Scanner(System.in);
 
 
-        FileInputStream fis=null;
-        try{
+        boolean option = true;
+
+        while (option) {
 
 
-         fis = new FileInputStream("./src/main/resources/goldilocks.txt");
-    } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("""
+                                \s
+                                What story would yoiu like to read?
+                                1. Goldilocks
+                                2. Hanzel and Gretel
+                                3. Mary Had a little lamp
+                    """);
+            option = false;
+
         }
-        Scanner fileScanner = new Scanner(fis);
+        int count = pScanner.nextInt();
+        pScanner.nextLine();
 
-        while(fileScanner.hasNextLine()){
+        switch (count) {
+            case 1:
+                readFile("Goldilocks.txt");
+            case 2:
+                readFile("hansel_and_gretel.txt");
+            case 3:
+                readFile("mary_had_a_little_lamb.txt");
+        }
+
+    }
+
+    private static void readFile(String filename) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream("./src/main/resources/" + filename);
+        Scanner fileScanner = new Scanner(fis);
+        while (fileScanner.hasNextLine()) {
             String input = fileScanner.nextLine();
             System.out.println(input);
         }
+        fileScanner.close();
+
     }
 }
